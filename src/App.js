b1aa89fa4a7c54e4b296779d9e6b17af8d9dec3b
cdev2024 <creativedev2024@gmail.com>
2024-11-26 11:14:06 +0900

    [2024-11-26] first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginUser from "./components/LoginUser";
+import RegisterUser from "./components/RegisterUser";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* 기본 라우트 : 로그인 화면 */}
+        <Route path="/" element={<Navigate to="/user/login" />} />
+
+        {/* 로그인 및 회원 가입 */}
+        <Route path="/user/login" element={<LoginUser />} />
+        <Route path="/user/register" element={<RegisterUser />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
